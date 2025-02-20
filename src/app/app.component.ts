d7e2f27d526f7ac44cc63c1f968e1f360d7391f4
AJ Marcinek <abmarcinek@gmail.com>
2025-02-20 12:23:49 -0600

    updated implementation from new README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { EmailEditorComponent, EmailEditorModule } from 'angular-email-editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [EmailEditorModule],
 })
 export class AppComponent {
-  title = 'unlayer-ang';
+  title = 'angular-email-editor';
+
+  @ViewChild(EmailEditorComponent)
+  private emailEditor!: EmailEditorComponent;
+
+  private get unlayer() {
+    return this.emailEditor.editor;
+  }
+
+  // called when the editor is created
+  editorLoaded() {
+    console.log('editorLoaded');
+    // load the design json here
+    // you can get the design json by calling unlayer.exportHtml (see below)
+    // this.unlayer.loadDesign({ /* json object here */ });
+  }
+
+  // called when the editor has finished loading
+  editorReady() {
+    console.log('editorReady');
+  }
+
+  exportHtml() {
+    this.unlayer.exportHtml((result) => {
+      // result object format: { html: string, design: object, amp: object, chunks: object }
+      console.log('exportHtml', result);
+    });
+  }
 }
