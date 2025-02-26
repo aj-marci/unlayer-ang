8f8d466acc7ba5bbb49ebeea46944e70793c3141
AJ Marcinek <abmarcinek@gmail.com>
2025-02-26 09:56:51 -0600

    passing properties and custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { EmailEditorComponent, EmailEditorModule } from 'angular-email-editor';
 
 @Component({
@@ -7,21 +7,56 @@ import { EmailEditorComponent, EmailEditorModule } from 'angular-email-editor';
   styleUrls: ['./app.component.css'],
   imports: [EmailEditorModule],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-email-editor';
   projectId = 239206;
-  editorOptions = {
+  editorOptions: EmailEditorComponent['options'] = {
     displayMode: "email",
     appearance: {
-        theme: "modern_dark",
-      },
+      theme: "modern_dark",
+    },
   };
 
-  @ViewChild(EmailEditorComponent)
+  @ViewChild(EmailEditorComponent, { static: false })
   private emailEditor!: EmailEditorComponent;
 
   private get unlayer() {
-    return this.emailEditor.editor;
+    return this.emailEditor?.editor;
+  }
+
+  ngAfterViewInit() {
+    if (this.unlayer) {
+      // Ensure the editor is fully loaded before setting up the validator
+      this.unlayer.addEventListener('editor:ready', () => this.editorReady());
+    }
+  }
+
+  // called when the editor has finished loading
+  editorReady() {
+    console.log('editorReady');
+
+    if (this.unlayer) {
+
+      this.unlayer.setToolValidator('text', async (data: any) => {
+        const { defaultErrors, values } = data;
+      
+        if (values.text.length > 400) {
+          return Promise.resolve([
+            {
+              id: 'CHAR_COUNT_ERROR',
+              icon: 'fa-exclamation-triangle',
+              severity: 'ERROR',
+              title: 'Character Count Exceeded',
+              description: `A text block exceeds 400 characters. Current length: ${values.text.length}. Please break up into separate text blocks.`,
+              dismissable: true // true/false boolean
+            },
+            ...defaultErrors,
+          ]);
+        }
+      
+        return Promise.resolve(defaultErrors);
+      });
+    }
   }
 
   // called when the editor is created
@@ -32,13 +67,8 @@ export class AppComponent {
     // this.unlayer.loadDesign({ /* json object here */ });
   }
 
-  // called when the editor has finished loading
-  editorReady() {
-    console.log('editorReady');
-  }
-
   exportHtml() {
-    this.unlayer.exportHtml((result) => {
+    this.unlayer?.exportHtml((result: any) => {
       // result object format: { html: string, design: object, amp: object, chunks: object }
       console.log('exportHtml', result);
     });
